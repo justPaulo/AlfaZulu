061fb40437d66d09d22eeec6b3d39ce53e6536a6
justPaulo <pnascim@gmail.com>
2018-12-28 15:17:06 +0000

    Initial commit
    Initial commit from earlier project dated 2006

---
+++ AlfaZulu/AlfaZulu.h
@@ -0,0 +1,11 @@
+/* AlfaZulu */
+
+#import <Cocoa/Cocoa.h>
+
+@interface AlfaZulu : NSObject
+{
+    IBOutlet NSTextField *input;
+    IBOutlet NSTextField *output;
+}
+- (IBAction)Processar:(id)sender;
+@end
---
+++ AlfaZulu/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  AlfaZulu
+//
+//  Created by Paulo on Fri Sep 01 2006.
+//  Copyright (c) 2006 Paulo Nascimento. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char *argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
